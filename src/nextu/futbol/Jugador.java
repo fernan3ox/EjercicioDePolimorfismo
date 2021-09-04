b7b1512f8430820aa1c5499c34078f849c601814
fernan3ox <fernandoramiro21@gmail.com>
2021-09-04 02:23:27 -0300

    first commit

---
+++ src/nextu/futbol/Jugador.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nextu.futbol;
+
+/**
+ *
+ * @author Fernandito
+ */
+public class Jugador {
+    
+    private String nombre;
+    private int numerodecamiseta;
+    
+    public void jugar(){
+        System.out.println("Nombre del jugador es :"+nombre);
+        System.out.println("Numero de Camiseta es : "+numerodecamiseta);
+            
+        
+    }
+}
